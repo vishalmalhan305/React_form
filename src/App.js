66c12402ddfba25922ecd4b5833717fd07c1004e
vishalmalhan305 <135173838+vishalmalhan305@users.noreply.github.com>
2023-10-17 19:24:29 -0400

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import NewShop from './components/NewShop';
+import NewProduct from './components/NewProduct';
+
 
 
 function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <Router>
+    <h1> Hello World </h1>
+
+
+    <Routes>
+      <Route path='navbar' Component={NavBar}></Route>
+      <Route path="newshop" Component={NewShop}/>
+      <Route path="newproduct" Component={NewProduct}/>
+    </Routes>
+   
+
+    </Router>
   );
 }
 
